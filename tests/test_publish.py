ad324108128b798ce15ced22d40666114875e4ba
Manu <4445816+tr4nt0r@users.noreply.github.com>
2025-03-30 16:23:54 +0200

    Add support for authenticated publishing and subscribing (#29)

--- tests/test_publish.py
+++ tests/test_publish.py
@@ -23,6 +23,75 @@ async def test_publish_message(mock_session: AsyncMock) -> None:
     mock_session.request.assert_called_once_with(
         "POST",
         URL("http://example.com"),
+        headers=None,
+        json={
+            "topic": "mytopic",
+            "message": "This is a test message",
+            "title": "Test",
+            "tags": [],
+            "priority": None,
+            "actions": [],
+            "click": None,
+            "attach": None,
+            "markdown": False,
+            "icon": None,
+            "filename": None,
+            "delay": None,
+            "email": None,
+            "call": None,
+        },
+    )
+
+
+async def test_publish_basic_auth(mock_session: AsyncMock) -> None:
+    """Test publishing a message to ntfy with basic authentication."""
+
+    mock_session.request.return_value.__aenter__.return_value.status = 200
+    mock_session.request.return_value.__aenter__.return_value.text.return_value = MSG
+
+    message = Message(topic="mytopic", title="Test", message="This is a test message")
+    ntfy = Ntfy("http://example.com", mock_session, username="user", password="pass")  # noqa: S106
+
+    await ntfy.publish(message)
+
+    mock_session.request.assert_called_once_with(
+        "POST",
+        URL("http://example.com"),
+        headers={"Authorization": "Basic dXNlcjpwYXNz"},
+        json={
+            "topic": "mytopic",
+            "message": "This is a test message",
+            "title": "Test",
+            "tags": [],
+            "priority": None,
+            "actions": [],
+            "click": None,
+            "attach": None,
+            "markdown": False,
+            "icon": None,
+            "filename": None,
+            "delay": None,
+            "email": None,
+            "call": None,
+        },
+    )
+
+
+async def test_publish_bearer_auth(mock_session: AsyncMock) -> None:
+    """Test publishing a message to ntfy with bearer authentication."""
+
+    mock_session.request.return_value.__aenter__.return_value.status = 200
+    mock_session.request.return_value.__aenter__.return_value.text.return_value = MSG
+
+    message = Message(topic="mytopic", title="Test", message="This is a test message")
+    ntfy = Ntfy("http://example.com", mock_session, token="dXNlcjpwYXNz")  # noqa: S106
+
+    await ntfy.publish(message)
+
+    mock_session.request.assert_called_once_with(
+        "POST",
+        URL("http://example.com"),
+        headers={"Authorization": "Bearer dXNlcjpwYXNz"},
         json={
             "topic": "mytopic",
             "message": "This is a test message",
