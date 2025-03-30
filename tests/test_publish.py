0b34986c98b919e9e9f0f0bd213d002a905f200f
Manu <4445816+tr4nt0r@users.noreply.github.com>
2025-03-30 16:14:58 +0200

    Refactor tests for publish method and remove aioresponses dependency (#28)

--- tests/test_publish.py
+++ tests/test_publish.py
@@ -1,38 +1,42 @@
 """Tests for aiontfy."""
 
-from dataclasses import asdict
+from unittest.mock import AsyncMock
 
-from aiohttp import ClientSession
-from aioresponses import aioresponses
+from yarl import URL
 
 from aiontfy import Message, Ntfy
 
+from .conftest import MSG
 
-async def test_publish_message(aioclient_mock: aioresponses) -> None:
+
+async def test_publish_message(mock_session: AsyncMock) -> None:
     """Test publishing a message to ntfy."""
 
-    aioclient_mock.post(
-        "http://example.com",
-        status=200,
-        payload={
-            "id": "12345",
-            "time": 1736288009,
-            "expires": 1736331209,
-            "event": "message",
+    mock_session.request.return_value.__aenter__.return_value.status = 200
+    mock_session.request.return_value.__aenter__.return_value.text.return_value = MSG
+
+    message = Message(topic="mytopic", title="Test", message="This is a test message")
+    ntfy = Ntfy("http://example.com", mock_session)
+
+    await ntfy.publish(message)
+
+    mock_session.request.assert_called_once_with(
+        "POST",
+        URL("http://example.com"),
+        json={
             "topic": "mytopic",
-            "title": "Title",
             "message": "This is a test message",
-            "content_type": "text/markdown",
+            "title": "Test",
+            "tags": [],
+            "priority": None,
+            "actions": [],
+            "click": None,
+            "attach": None,
+            "markdown": False,
+            "icon": None,
+            "filename": None,
+            "delay": None,
+            "email": None,
+            "call": None,
         },
-    )
-    async with ClientSession() as session:
-        message = Message(
-            topic="mytopic", title="Test", message="This is a test message"
-        )
-        ntfy = Ntfy("http://example.com", session)
-
-        await ntfy.publish(message)
-
-    aioclient_mock.assert_called_once_with(
-        "http://example.com", method="POST", json=asdict(message)
     )
