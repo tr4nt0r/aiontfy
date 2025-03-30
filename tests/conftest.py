bd97d156224d098961f12f9af1407131b8b3404b
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-03-30 16:12:35 +0200

    Refactor tests for publish method and remove aioresponses dependency

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,18 +5,10 @@ from unittest.mock import AsyncMock, MagicMock
 
 from aiohttp import ClientResponse, ClientSession, WSMsgType
 from aiohttp.web_ws import WebSocketResponse
-from aioresponses import aioresponses
 import pytest
 
 MSG = """{"id": "h6Y2hKA5sy0U", "time": 1743184726, "expires": 1743227926, "event": "message", "topic": "test1", "message": "Hello", "title": "Title", "tags": ["octopus"], "priority": 3, "click": "https://example.com/", "actions": [], "attachment": null}"""
 MSG_2 = """{"id": "h6Y2hKA5sy0U", "time": 1743184726, "expires": 1743227926, "event": "message", "topic": "test2", "message": "World", "title": "Title", "tags": ["octopus"], "priority": 5, "click": "https://example.com/", "actions": [], "attachment": null}"""
-
-
-@pytest.fixture(autouse=True)
-def aioclient_mock() -> Generator[aioresponses]:
-    """Mock Aiohttp client requests."""
-    with aioresponses() as m:
-        yield m
 
 
 @pytest.fixture
